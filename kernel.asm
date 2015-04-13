eaf70e73a864f244e603525e1736446e4f883531
Tabeth Nkangoh 15 <vega.cs.amherst.edu>
2015-04-12 22:55:39 -0400

    Beginning combining kernel stub with our code. To complete soon

--- kernel.asm
+++ kernel.asm
@@ -20,10 +20,203 @@
 	SETTBR +TT_base
 	SETIBR +IB_IP
 
+
+
+;;; ================================================================================================================================
+;;; Procedure: print
+;;; Callee preserved registers:
+;;;   [%FP - 4]: G0
+;;;   [%FP - 8]: G3
+;;;   [%FP - 12]: G4
+;;; Parameters:
+;;;   [%FP + 0]: A pointer to the beginning of a null-terminated string.
+;;; Caller preserved registers:
+;;;   [%FP + 4]: FP
+;;; Return address:
+;;;   [%FP + 8]
+;;; Return value:
+;;;   <none>
+;;; Locals:
+;;;   %G0: Pointer to the current position in the string.
+	
+_procedure_print:
+
+	;; Prologue: Push preserved registers.
+	SUBUS		%SP		%SP		4
+	COPY		*%SP		%G0
+	SUBUS		%SP		%SP		4
+	COPY		*%SP		%G3
+	SUBUS		%SP		%SP		4
+	COPY		*%SP		%G4
+
+	;; If not yet initialized, set the console base/limit statics.
+	BNEQ		+print_init_loop	*+_static_console_base		0
+	SUBUS		%SP		%SP		12		; Push pfp / ra / rv
+	COPY		*%SP		%FP				; pFP = %FP
+	SUBUS		%SP		%SP		4 		; Push arg[1]
+	COPY		*%SP		1				; Find the 1st device of the given type.
+	SUBUS		%SP		%SP		4		; Push arg[0]
+	COPY		*%SP		*+_static_console_device_code	; Find a console device.
+	COPY		%FP		%SP				; Update %FP
+	ADDUS		%G5		%SP		12		; %G5 = &ra
+	CALL		+_procedure_find_device		*%G5
+	ADDUS		%SP		%SP		8 		; Pop arg[0,1]
+	COPY		%FP		*%SP 				; %FP = pfp
+	ADDUS		%SP		%SP		8		; Pop pfp / ra
+	COPY		%G4		*%SP				; %G4 = &dt[console]
+	ADDUS		%SP		%SP		4		; Pop rv
+
+	;; Panic if the console was not found.
+	BNEQ		+print_found_console	%G4		0
+	COPY		%G5		*+_static_kernel_error_console_not_found
+	HALT
+	
+print_found_console:	
+	ADDUS		%G3		%G4		*+_static_dt_base_offset  ; %G3 = &console[base]
+	COPY		*+_static_console_base		*%G3			  ; Store static console[base]
+	ADDUS		%G3		%G4		*+_static_dt_limit_offset ; %G3 = &console[limit]
+	COPY		*+_static_console_limit		*%G3			  ; Store static console[limit]
+	
+print_init_loop:	
+
+	;; Loop through the characters of the given string until the null character is found.
+	COPY		%G0		*%FP 				; %G0 = str_ptr
+print_loop_top:
+	COPYB		%G4		*%G0 				; %G4 = current_char
+
+	;; The loop should end if this is a null character
+	BEQ		+print_loop_end	%G4		0
+
+	;; Scroll without copying the character if this is a newline.
+	COPY		%G3		*+_static_newline_char		; %G3 = <newline>
+	BEQ		+print_scroll_call	%G4	%G3
+
+	;; Assume that the cursor is in a valid location.  Copy the current character into it.
+	;; The cursor position c maps to buffer location: console[limit] - width + c
+	SUBUS		%G3		*+_static_console_limit	*+_static_console_width	   ; %G3 = console[limit] - width
+	ADDUS		%G3		%G3		*+_static_cursor_column		   ; %G3 = console[limit] - width + c
+	COPYB		*%G3		%G4						   ; &(height - 1, c) = current_char
+	
+	;; Advance the cursor, scrolling if necessary.
+	ADD		*+_static_cursor_column	*+_static_cursor_column		1	; c = c + 1
+	BLT		+print_scroll_end	*+_static_cursor_column	*+_static_console_width	; Skip scrolling if c < width
+	;; Fall through...
+	
+print_scroll_call:	
+	SUBUS		%SP		%SP		8				; Push pfp / ra
+	COPY		*%SP		%FP						; pfp = %FP
+	COPY		%FP		%SP						; %FP = %SP
+	ADDUS		%G5		%FP		4				; %G5 = &ra
+	CALL		+_procedure_scroll_console	*%G5
+	COPY		%FP		*%SP 						; %FP = pfp
+	ADDUS		%SP		%SP		8				; Pop pfp / ra
+
+print_scroll_end:
+	;; Place the cursor character in its new position.
+	SUBUS		%G3		*+_static_console_limit		*+_static_console_width ; %G3 = console[limit] - width
+	ADDUS		%G3		%G3		*+_static_cursor_column	        ; %G3 = console[limit] - width + c	
+	COPY		%G4		*+_static_cursor_char				        ; %G4 = <cursor>
+	COPYB		*%G3		%G4					        ; console@cursor = <cursor>
+	
+	;; Iterate by advancing to the next character in the string.
+	ADDUS		%G0		%G0		1
+	JUMP		+print_loop_top
+
+print_loop_end:
+	;; Epilogue: Pop and restore preserved registers, then return.
+	COPY		%G4		*%SP
+	ADDUS		%SP		%SP		4
+	COPY		%G3		*%SP
+	ADDUS		%SP		%SP		4
+	COPY		%G0		*%SP
+	ADDUS		%SP		%SP		4
+	ADDUS		%G5		%FP		8 		; %G5 = &ra
+	JUMP		*%G5
+;;; ================================================================================================================================
+;;; Procedure: scroll_console
+;;; Description: Scroll the console and reset the cursor at the 0th column.
+;;; Callee reserved registers:
+;;;   [%FP - 4]:  G0
+;;;   [%FP - 8]:  G1
+;;;   [%FP - 12]: G4
+;;; Parameters:
+;;;   <none>
+;;; Caller preserved registers:
+;;;   [%FP + 0]:  FP
+;;; Return address:
+;;;   [%FP + 4]
+;;; Return value:
+;;;   <none>
+;;; Locals:
+;;;   %G0:  The current destination address.
+;;;   %G1:  The current source address.
+
+
+;;; ================================================================================================================================
+
+_procedure_scroll_console:
+
+	;; Prologue: Push preserved registers.
+	SUBUS		%SP		%SP		4
+	COPY		*%SP		%G0
+	SUBUS		%SP		%SP		4
+	COPY		*%SP		%G1
+	SUBUS		%SP		%SP		4
+	COPY		*%SP		%G4
+
+	;; Initialize locals.
+	COPY		%G0		*+_static_console_base			   ; %G0 = console[base]
+	ADDUS		%G1		%G0		*+_static_console_width	   ; %G1 = console[base] + width
+
+	;; Clear the cursor.
+	SUBUS		%G4		*+_static_console_limit		*+_static_console_width ; %G4 = console[limit] - width
+	ADDUS		%G4		%G4		*+_static_cursor_column			; %G4 = console[limit] - width + c
+	COPYB		*%G4		*+_static_space_char					; Clear cursor.
+
+	;; Copy from the source to the destination.
+	;;   %G3 = DMA portal
+	;;   %G4 = DMA transfer length
+	ADDUS		%G3		8		*+_static_device_table_base ; %G3 = &controller[limit]
+	SUBUS		%G3		*%G3		12                          ; %G3 = controller[limit] - 3*|word| = &DMA_portal
+	SUBUS		%G4		*+_static_console_limit	%G0 		    ; %G4 = console[base] - console[limit] = |console|
+	SUBUS		%G4		%G4		*+_static_console_width     ; %G4 = |console| - width
+
+	;; Copy the source, destination, and length into the portal.  The last step triggers the DMA copy.
+	COPY		*%G3		%G1 					; DMA[source] = console[base] + width
+	ADDUS		%G3		%G3		4 			; %G3 = &DMA[destination]
+	COPY		*%G3		%G0 					; DMA[destination] = console[base]
+	ADDUS		%G3		%G3		4 			; %G3 = &DMA[length]
+	COPY		*%G3		%G4 					; DMA[length] = |console| - width; DMA trigger
+
+	;; Perform a DMA transfer to blank the last line with spaces.
+	SUBUS		%G3		%G3		8 			; %G3 = &DMA_portal
+	COPY		*%G3		+_string_blank_line			; DMA[source] = &blank_line
+	ADDUS		%G3		%G3		4 			; %G3 = &DMA[destination]
+	SUBUS		*%G3		*+_static_console_limit	*+_static_console_width	; DMA[destination] = console[limit] - width
+	ADDUS		%G3		%G3		4 			; %G3 = &DMA[length]
+	COPY		*%G3		*+_static_console_width			; DMA[length] = width; DMA trigger
+	
+	;; Reset the cursor position.
+	COPY		*+_static_cursor_column		0			                ; c = 0
+	SUBUS		%G4		*+_static_console_limit		*+_static_console_width ; %G4 = console[limit] - width
+	COPYB		*%G4		*+_static_cursor_char				   	; Set cursor.
+	
+	;; Epilogue: Pop and restore preserved registers, then return.
+	COPY		%G4		*%SP
+	ADDUS		%SP		%SP		4
+	COPY		%G1		*%SP
+	ADDUS		%SP		%SP		4
+	COPY		%G0		*%SP
+	ADDUS		%SP		%SP		4
+	ADDUS		%G5		%FP		4 		; %G5 = &ra
+	JUMP		*%G5
+;;; ================================================================================================================================
+
 ;;; Find next ROM device
 ;;; Registers: G0 = address of next device in BC
 ;;; 	       G1 = device value, G2 = boolean 2 found at bus_index
 ;;; 	       G3 = two_count
+
 	COPY %G0 *+bus_index
 	COPY %G3 0
 ;;; Step 16 from top
@@ -82,25 +275,45 @@ handler:
 	HALT
 
 .Numeric
-kernel_base:	0
-kernel_limit:	0
+_static_kernel_base:	0
+_static_kernel_limit:	0
 process_base:	
 IB_IP:	0
 IB_MISC:	0
 bus_index:	 0x00001000
+
+;; Kernel Error Codes
+_static_kernel_error_RAM_not_found:	0xffff0001
+_static_kernel_error_main_returned:	0xffff0002
+_static_kernel_error_small_RAM:		0xffff0003	
+_static_kernel_error_console_not_found:	0xffff0004
+
+;; Console management
+_static_console_width:		80
+_static_console_height:		24
+_static_space_char:		0x20202020 ; Four copies for faster scrolling.  If used with COPYB, only the low byte is used.
+_static_cursor_char:		0x5f
+_static_newline_char:		0x0a
+
+
+;; Trap Table --
 TT_base:
-INVALID_ADDRESS:	0
-INVALID_REGISTER:	0
-BUS_ERROR: 	0
-CLOCK_ALARM:	0
-DIVIDE_BY_ZERO:	0
-OVERFLOW:	0
-INVALID_INSTRUCTION:	0
-PERMISSION_VIOLATION:	0
-INVALID_SHIFT_AMOUNT:	0
-SYSTEM_CALL:	0
-INVALID_DEVICE_VALUE:	0
-DEVICE_FAILURE:	0
+	INVALID_ADDRESS:	0
+	INVALID_REGISTER:	0
+	BUS_ERROR: 	0
+	CLOCK_ALARM:	0
+	DIVIDE_BY_ZERO:	0
+	OVERFLOW:	0
+	INVALID_INSTRUCTION:	0
+	PERMISSION_VIOLATION:	0
+	INVALID_SHIFT_AMOUNT:	0
+	SYSTEM_CALL:	0
+	INVALID_DEVICE_VALUE:	0
+	DEVICE_FAILURE:	0
+;; --
 	
-	
+.Text
+_string_done_msg: "done. \n"
+_string_abort_msg: "failed! Halting now.\n"
+_string_blank_link : "	
 	
